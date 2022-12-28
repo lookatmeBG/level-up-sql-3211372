fdf6cd6a14701786b7b037e0dfafd62d95e50231
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 14:19:38 +0000

    01_06 challenges

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,10 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT * FROM Reservations r 
+join Customers c on c.CustomerID = r.CustomerID
+WHERE c.FirstName = 'Norby' and r.Date > '2022-07-24';
+
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
