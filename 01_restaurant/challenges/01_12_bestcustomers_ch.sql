fe4f2efafdffba404a2295886c7a2dab9e115524
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 16:23:50 +0000

    01_12 challenges

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,15 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT
+c.FirstName
+, c.LastName
+, c.Email
+, COUNT(*) as OrderCount
+FROM
+Customers c 
+join Orders o on o.CustomerID = c.CustomerID
+Group by o.CustomerID
+Having COUNT(*) > 14
+Order by COUNT(*) DESC;
