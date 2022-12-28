87cfac597a6135c8840ec3ba757b4bf2f702f418
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 15:35:32 +0000

    01_09 challenges

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,12 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+
+select * from customers where Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Customers ( FirstName, LastName, Email, Phone)
+VALUES ('Sam', 'McAdams', 'smac@kinetecoinc.com', '(555) 555-1232' );
+
+insert into Reservations (CustomerID, Date, PartySize)
+VALUES ( 102, '2022-08-12 18:00:00', 5);
