ea441838b27989f5a3449ea556e66155da140542
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 13:48:54 +0000

    01_03 challenges

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,28 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT 
+Type
+, Name
+, Price
+, Description 
+FROM Dishes
+ORDER BY Price ASC;
+
 -- Create a report showing appetizers and beverages.
+SELECT
+Type
+, Name
+, Price
+, Description 
+FROM Dishes
+WHERE Type in ('Appetizer', 'Beverage' );
+
 -- Create a report with all items except beverages.
+SELECT
+Type
+, Name
+, Price
+, Description 
+FROM Dishes
+WHERE type not in ( 'Beverage' );
