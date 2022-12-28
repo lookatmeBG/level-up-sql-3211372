c4d51977e31b7f47010303896011ae90fc5dbcb3
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 16:07:33 +0000

    01_10 Challenges

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,26 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+select * from Customers where FirstName = 'Loretta' and LastName = 'Hundey' and Address = '6939 Elka Place';
+
+INSERT INTO Orders ( CustomerID, OrderDate) VALUES (70, '2022-09-20 14:00:00');
+
+SELECT * from Orders where OrderDate = '2022-09-20 14:00:00';
+
+Select * from Dishes where Name in ('House Salad', 'Mini Cheeseburgers', 'Tropical Blue Smoothie');
+
+INSERT INTO OrdersDishes ( OrderID, DishID ) VALUES
+( 1001, (Select DishID from Dishes where Name = 'House Salad')),
+( 1001, (Select DishID from Dishes where Name = 'Mini Cheeseburgers')),
+( 1001, (Select DishID from Dishes where Name = 'Tropical Blue Smoothie'));
+
+SELECT * FROM Dishes
+join OrdersDishes on Dishes.DishID = OrdersDishes.DishID
+WHERE OrdersDishes.OrderID = 1001;
+
+SELECT
+SUM( d.Price)
+FROM OrdersDishes od 
+Join Dishes d on d.DishID = od.DishID 
+WHERE od.OrderID = 1001;
