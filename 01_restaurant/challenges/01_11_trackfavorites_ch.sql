e2df95900a35423fc8bdd1754a0c4e766051483e
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 16:14:57 +0000

    01_11 challenges

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,12 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers where FirstName = 'Cleo' and LastName = 'Goldwater';
+SELECT * FROM Dishes where Name = 'Quinoa Salmon Salad';
+
+UPDATE Customers set FavoriteDish = 9 where CustomerID = 42;
+
+SELECT c.FirstName, c.LastName, c.FavoriteDish, d.Name
+FROM Customers c 
+join Dishes d on c.FavoriteDish = d.DishID;
