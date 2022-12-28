27792b50b27e41c718764492958319bae3c0bc17
lookatmeBG <look_at_me1@abv.bg>
2022-12-28 14:38:22 +0000

    01_07 challenges

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,9 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+SELECT * FROM CUSTOMERS where Email = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO anniversary VALUES (92, 4);
+
+SELECT * FROM anniversary;
