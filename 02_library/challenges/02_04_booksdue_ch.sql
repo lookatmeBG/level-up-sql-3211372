ebf2ba9848ae3975cd2551bea6aecdcf56e15365
lookatmeBG <look_at_me1@abv.bg>
2022-12-29 08:23:11 +0000

    02_04 challenges

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,10 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT 
+l.DueDate, b.Title, p.FirstName, p.Email
+FROM Loans l 
+join Books b on b.BookID = l.BookID
+join Patrons p on p.PatronID = l.PatronID
+WHERE l.DueDate = '2022-07-13' and l.ReturnedDate IS NULL;
