d8edd6fd5214ca33509ff5ddb69dfde154f0a7e6
lookatmeBG <look_at_me1@abv.bg>
2022-12-29 09:09:54 +0000

    02_08 challenges

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,21 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT
+b.Published
+, COUNT(DISTINCT(b.Title)) as BookCount
+FROM Books b
+GROUP BY b.Published
+ORDER BY BookCount DESC;
+
+SELECT
+b.Title
+, b.Barcode
+, COUNT(l.BookID) as BookCount
+FROM Loans l 
+JOIN Books b on b.BookID = l.BookID
+WHERE l.ReturnedDate IS NOT NULL
+GROUP BY b.Title
+ORDER BY BookCount DESC
+LIMIT 5;
