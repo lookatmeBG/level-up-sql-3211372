8eb2d8542e653ca2de71f3c78c9be68838700061
lookatmeBG <look_at_me1@abv.bg>
2022-12-29 08:54:44 +0000

    02_07 challenges

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,25 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT
+b.Title
+, b.Author
+, b.Published
+, b.Barcode
+FROM Books b 
+WHERE b.Published BETWEEN 1890 and 1899
+AND not exists ( SELECT 1 FROM Loans l where l.BookID = b.BookID AND l.ReturnedDate IS NULL )
+ORDER BY b.Title;
+
+-- OR
+
+SELECT
+b.Title
+, b.Author
+, b.Published
+, b.Barcode
+FROM Books b 
+WHERE b.Published BETWEEN 1890 and 1899
+AND b.BookID not in( SELECT l.BookID FROM Loans l where l.ReturnedDate IS NULL )
+ORDER BY b.Title;  
