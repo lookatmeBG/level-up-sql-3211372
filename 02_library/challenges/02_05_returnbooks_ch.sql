01b06a8f7c44c47aff97a0b3bf31029325a8a11d
lookatmeBG <look_at_me1@abv.bg>
2022-12-29 08:32:13 +0000

    02_05 challenges

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,18 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+
+SELECT l.LoanID
+FROM Loans l
+JOIN Books b on b.BookID = l.BookID 
+WHERE b.Barcode in (6435968624,5677520613,8730298424) AND l.ReturnedDate IS NULL;
+
+UPDATE Loans set ReturnedDate = '2022-07-05' WHERE  LoanID in (1991,1992,1999);
+--OR
+UPDATE Loans set ReturnedDate = '2022-07-05' WHERE  LoanID in (
+  SELECT l.LoanID
+  FROM Loans l
+  JOIN Books b on b.BookID = l.BookID 
+  WHERE b.Barcode in (6435968624,5677520613,8730298424) AND l.ReturnedDate IS NULL
+);
