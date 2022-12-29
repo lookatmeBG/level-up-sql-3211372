c634819e68837ee5f75d2fb63884392b7f430068
lookatmeBG <look_at_me1@abv.bg>
2022-12-29 08:38:57 +0000

    02_06 challenges

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,10 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT
+COUNT(l.BookID) as BookCount
+, p.FirstName, p.Email
+FROM Loans l 
+JOIN Patrons p on p.PatronID = l.PatronID
+GROUP BY l.PatronID
+ORDER BY BookCount ASC;
